920981551a10df0cb25eee576db97119cc41a140
Dmytro Shevchuk <dimonshevchuk1@gmail.com>
2026-02-26 12:31:11 +0200

    Parallel vers

--- main.cpp
+++ main.cpp
@@ -35,6 +35,45 @@ void sequentail(Matrix& matrix, int N)
     }
 }
 
+void processColumns(Matrix& matrix, int N, int start, int end)
+{
+    for (int j = start; j < end; j++)
+    {
+        int min_val = matrix[0][j];
+
+        for (int i = 1; i < N; i++)
+        {
+            if (matrix[i][j] < min_val)
+                min_val = matrix[i][j];
+        }
+
+        matrix[N - 1 - j][j] = min_val;
+    }
+}
+
+void parallel(Matrix& matrix, int N, int num_threads)
+{
+    vector<thread> threads;
+
+    int chunk = N / num_threads;
+
+    for (int t = 0; t < num_threads; t++)
+    {
+        int start = t * chunk;
+        int end = (t == num_threads - 1) ? N : start + chunk;
+
+        threads.emplace_back(processColumns,
+                             ref(matrix),
+                             N,
+                             start,
+                             end);
+    }
+
+    for (auto& th : threads)
+        th.join();
+}
+
+
 
 int main()
 {
