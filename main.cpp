e3f982d2163ca996a19c9687412aa5fa89017507
Dmytro Shevchuk <dimonshevchuk1@gmail.com>
2026-03-09 12:12:09 +0200

    fillarray

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+using Clock = chrono::high_resolution_clock;
+
+void fillArray(vector<int>& data)
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1,1000);
+
+    for(auto &v : data)
+        v = dist(gen);
+}
+
+
+int main()
+{
+
+  return 0;
+}
